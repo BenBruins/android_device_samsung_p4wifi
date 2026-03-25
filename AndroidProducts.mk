f36fc8f5ad1977156cbb163184ef3357ecac06b2
BenBruins <ben.bruins@home.nl>
2026-03-25 12:38:21 +0100

    Fix formatting of PRODUCT_MAKEFILES in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,5 @@
 # limitations under the License.
 #
 
-make
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/p4wifi.mk \
-    $(LOCAL_DIR)/lineage_p4wifi.mk
+    $(LOCAL_DIR)/p4wifi.mk \$(LOCAL_DIR)/lineage_p4wifi.mk
